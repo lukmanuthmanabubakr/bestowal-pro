b8b135c342bc482071f3914eee6bf33ee0c0730a
olaxkid 911 <lukmanuthmanabubakr>
2024-01-09 10:38:10 +0100

    update

--- client/src/components/navbar/NavbarMainHome.jsx
+++ client/src/components/navbar/NavbarMainHome.jsx
@@ -16,18 +16,20 @@ import "./navbarMainHome.css";
 const NavbarMainHome = () => {
   const { connect, address } = useStateContext();
   const [myTheme, setMyTheme] = useLocalStorage("theme" ? "dark" : "light");
-  const [active, setActive] = useState("bestowal-Features")
-  const [toggleIcon, setToggleIcon] = useState("nav_toggler")
+  const [active, setActive] = useState("bestowal-Features");
+  const [toggleIcon, setToggleIcon] = useState("nav_toggler");
 
   const navToggle = () => {
-    active === 'bestowal-Features' ? setActive('bestowal-Features nav_active') : setActive('bestowal-Features')
+    active === "bestowal-Features"
+      ? setActive("bestowal-Features nav_active")
+      : setActive("bestowal-Features");
 
     //Toggler Icon
 
-    toggleIcon === 'nav_toggler' ?  setToggleIcon('nav_toggler toggle') : setToggleIcon('nav_toggler')
-  }
-  
-
+    toggleIcon === "nav_toggler"
+      ? setToggleIcon("nav_toggler toggle")
+      : setToggleIcon("nav_toggler");
+  };
 
   const switchTheme = () => {
     const newTheme = myTheme === "light" ? "dark" : "light";
@@ -40,22 +42,78 @@ const NavbarMainHome = () => {
         <article className="bestowals-logo">
           <NavLink to="/">
             {" "}
-            <span>b</span> bestowal
+            <span>b</span> estowal
           </NavLink>
         </article>
         <article className={active}>
-          <NavLink to="/about"><span><TbWorldDollar /></span>About</NavLink>
-          <NavLink to="/features">  <span><RiEqualizerFill /></span> Features</NavLink>
-          <NavLink to="/possibility"> <span><MdGroups2 /></span> Possibility</NavLink>
-          <NavLink to="/blog"> <span><AiFillFund /></span> Library</NavLink>
-          <NavLink to="/contact"> <span><RiMailCheckLine /></span> Contact</NavLink>
+          <NavLink to="/about">
+            <span>
+              <TbWorldDollar />
+            </span>
+            About
+          </NavLink>
+          <NavLink to="/features">
+            {" "}
+            <span>
+              <RiEqualizerFill />
+            </span>{" "}
+            Features
+          </NavLink>
+          <NavLink to="/possibility">
+            {" "}
+            <span>
+              <MdGroups2 />
+            </span>{" "}
+            Possibility
+          </NavLink>
+          <NavLink to="/blog">
+            {" "}
+            <span>
+              <AiFillFund />
+            </span>{" "}
+            Library
+          </NavLink>
+          <NavLink to="/contact">
+            {" "}
+            <span>
+              <RiMailCheckLine />
+            </span>{" "}
+            Contact
+          </NavLink>
+
+          <div className="themeModeMedia">
+            {myTheme === "light" ? (
+              <MdOutlineWbSunny
+                className="sunIcon"
+                onClick={switchTheme}
+                size={25}
+              />
+            ) : (
+              <FaRegMoon className="sunIcon" onClick={switchTheme} size={25} />
+            )}
+            <div className="myCustomContainer">
+              <CustomButton
+                // btnType="button"
+                title={address ? "Create a campaign" : "Connect"}
+                styles={address ? "orange" : ""}
+                handleClick={() => {
+                  if (address) navigate("create-campaign");
+                  else connect();
+                }}
+              />
+            </div>
+          </div>
         </article>
       </div>
       <div className="themeMode">
         {myTheme === "light" ? (
-          <MdOutlineWbSunny className="sunIcon" onClick={switchTheme}  size={25}/>
+          <MdOutlineWbSunny
+            className="sunIcon"
+            onClick={switchTheme}
+            size={25}
+          />
         ) : (
-          <FaRegMoon className="sunIcon" onClick={switchTheme} size={25}/>
+          <FaRegMoon className="sunIcon" onClick={switchTheme} size={25} />
         )}
         <div className="myCustomContainer">
           <CustomButton
@@ -71,9 +129,9 @@ const NavbarMainHome = () => {
       </div>
 
       <div onClick={navToggle} className={toggleIcon}>
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
       </div>
     </nav>
   );
